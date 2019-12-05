3095bf9a37181ec2951ff67db267b4bbab06b07a
HyCraftHD <hycrafthd@live.de>
2019-12-05 12:56:59 +0100

    Add basic mod classes for loading the mod

---
+++ src/main/java/info/u_team/voice_chat/VoiceChatMod.java
@@ -0,0 +1,10 @@
+package info.u_team.voice_chat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VoiceChatMod.MODID)
+public class VoiceChatMod {
+	
+	public static final String MODID = "voicechat";
+	
+}
