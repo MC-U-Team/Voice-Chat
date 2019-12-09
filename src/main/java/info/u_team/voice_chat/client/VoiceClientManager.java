f8691ffacbea3a7733ecd76220b10ee29d9de60b
HyCraftHD <hycrafthd@live.de>
2019-12-09 16:11:46 +0100

    Fix timer crash

--- src/main/java/info/u_team/voice_chat/client/VoiceClientManager.java
+++ src/main/java/info/u_team/voice_chat/client/VoiceClientManager.java
@@ -11,13 +11,16 @@ public class VoiceClientManager {
 	
 	private static VoiceClient CLIENT;
 	
+	private static TimerTask TIMER_TASK;
+	
 	public static synchronized void start(int port, byte[] secret) {
 		try {
 			CLIENT = new VoiceClient(port, secret);
-			TIMER.schedule(new TimerTask() {
+			TIMER.schedule(TIMER_TASK = new TimerTask() {
 				
 				@Override
 				public void run() {
+					cancel();
 					TalkingList.removeAllThatAreInactiveFor200ms();
 				}
 			}, 200, 200);
@@ -31,7 +34,10 @@ public class VoiceClientManager {
 			CLIENT.close();
 			CLIENT = null;
 		}
-		TIMER.cancel();
+		if (TIMER_TASK != null) {
+			TIMER_TASK.cancel();
+			TIMER_TASK = null;
+		}
 	}
 	
 	public static synchronized void setHandshakeDone() {
