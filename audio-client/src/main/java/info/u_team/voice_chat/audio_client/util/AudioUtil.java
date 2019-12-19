e1db5782f1cd899d87444ac7d8c4955ac3e8555b
HyCraftHD <hycrafthd@live.de>
2019-12-19 13:19:38 +0100

    Add utility method

--- audio-client/src/main/java/info/u_team/voice_chat/audio_client/util/AudioUtil.java
+++ audio-client/src/main/java/info/u_team/voice_chat/audio_client/util/AudioUtil.java
@@ -1,5 +1,7 @@
 package info.u_team.voice_chat.audio_client.util;
 
+import java.util.*;
+
 import javax.sound.sampled.*;
 
 public class AudioUtil {
@@ -18,6 +20,17 @@ public class AudioUtil {
 			}
 		}
 		return defaultMixer;
+	}
+	
+	public static List<String> findAudioDevices(Line.Info lineInfo) {
+		final List<String> list = new ArrayList<>();
+		for (Mixer.Info mixerInfo : AudioSystem.getMixerInfo()) {
+			final Mixer mixer = AudioSystem.getMixer(mixerInfo);
+			if (mixer.isLineSupported(lineInfo)) {
+				list.add(mixerInfo.getName());
+			}
+		}
+		return list;
 	}
 	
 }
