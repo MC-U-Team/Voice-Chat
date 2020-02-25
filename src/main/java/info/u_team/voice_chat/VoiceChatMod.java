7abbeb467622a81c5442ab09f7ea7f00c8180d57
HyCraftHD <hycrafthd@live.de>
2020-02-25 18:28:14 +0100

    Remove test

--- src/main/java/info/u_team/voice_chat/VoiceChatMod.java
+++ src/main/java/info/u_team/voice_chat/VoiceChatMod.java
@@ -1,12 +1,10 @@
 package info.u_team.voice_chat;
 
 import org.apache.logging.log4j.*;
-import org.lwjgl.util.opus.Opus;
 
 import info.u_team.voice_chat.config.*;
 import info.u_team.voice_chat.dependency.DependencyManager;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
 
@@ -21,12 +19,6 @@ public class VoiceChatMod {
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
 		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
 		DependencyManager.construct();
-		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
-			int a = Opus.OPUS_SET_PHASE_INVERSION_DISABLED_REQUEST;
-			System.out.println("JAAAAAAAAAAAAAAAAAJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-			System.out.println(Opus.class);
-			System.out.println(a);
-		});
 	}
 	
 }
