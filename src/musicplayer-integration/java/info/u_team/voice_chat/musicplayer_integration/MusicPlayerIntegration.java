4462192207864ea71c5d02bf140f7a30d80cd323
HyCraftHD <hycrafthd@live.de>
2020-02-26 03:57:23 +0100

    Close encoder on musicplayer integration

--- src/musicplayer-integration/java/info/u_team/voice_chat/musicplayer_integration/MusicPlayerIntegration.java
+++ src/musicplayer-integration/java/info/u_team/voice_chat/musicplayer_integration/MusicPlayerIntegration.java
@@ -1,22 +1,29 @@
 package info.u_team.voice_chat.musicplayer_integration;
 
+import java.util.concurrent.*;
+
 import org.lwjgl.util.opus.Opus;
 
 import info.u_team.music_player.musicplayer.MusicPlayerManager;
 import info.u_team.voice_chat.api.IIntegration;
 import info.u_team.voice_chat.audio_client.api.opus.IOpusEncoder;
 import info.u_team.voice_chat.audio_client.opus.PcmOpusEncoder;
-import info.u_team.voice_chat.audio_client.util.EndianUtil;
+import info.u_team.voice_chat.audio_client.util.*;
 import info.u_team.voice_chat.client.*;
 import info.u_team.voice_chat.musicplayer_integration.message.MusicToServerPacket;
 import net.minecraft.client.Minecraft;
 
 public class MusicPlayerIntegration implements IIntegration {
 	
-	private final IOpusEncoder encoder = new PcmOpusEncoder(48000, 2, 20, 96000, Opus.OPUS_SIGNAL_MUSIC, 1000);
+	private ExecutorService executor;
+	
+	private IOpusEncoder encoder;
 	
 	@Override
 	public void start() {
+		executor = Executors.newSingleThreadExecutor(ThreadUtil.createDaemonFactory("music player sender"));
+		encoder = new PcmOpusEncoder(48000, 2, 20, 96000, Opus.OPUS_SIGNAL_MUSIC, 1000);
+		
 		MusicPlayerManager.getPlayer().setOutputConsumer((buffer, length) -> {
 			if (MusicPlayerManager.getPlayer().getVolume() == 0) {
 				return;
@@ -32,6 +39,8 @@ public class MusicPlayerIntegration implements IIntegration {
 	@Override
 	public void stop() {
 		MusicPlayerManager.getPlayer().setOutputConsumer(null);
+		executor.shutdown();
+		encoder.close();
 	}
 	
 }
