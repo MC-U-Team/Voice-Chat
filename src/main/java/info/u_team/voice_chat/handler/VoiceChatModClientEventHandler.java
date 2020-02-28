118e62d25ba234a4e916934f88f7ea70a67cd2cb
HyCraftHD <hycrafthd@live.de>
2020-02-28 14:49:17 +0100

    Add button to open voicechat settings (wip)

--- src/main/java/info/u_team/voice_chat/handler/VoiceChatModClientEventHandler.java
+++ src/main/java/info/u_team/voice_chat/handler/VoiceChatModClientEventHandler.java
@@ -1,10 +1,13 @@
 package info.u_team.voice_chat.handler;
 
+import info.u_team.u_team_core.gui.elements.BetterButton;
 import info.u_team.voice_chat.VoiceChatMod;
+import info.u_team.voice_chat.musicplayer_integration.util.MusicPlayerSettingsUtil;
 import info.u_team.voice_chat.render.RenderOverlayVoiceChat;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.*;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.*;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -22,4 +25,13 @@ public class VoiceChatModClientEventHandler {
 		RenderOverlayVoiceChat.draw();
 	}
 	
+	@SubscribeEvent
+	public static void on(GuiScreenEvent.InitGuiEvent.Post event) {
+		final Screen gui = event.getGui();
+		if (gui instanceof IngameMenuScreen) {
+			event.addWidget(new BetterButton(gui.width - 103, 1 + (MusicPlayerSettingsUtil.isShowIngameMenueOverlay() ? 15 : 0), 102, 15, 0.7F, "Voicechat", button -> {
+				System.out.println("PRESSED");
+			}));
+		}
+	}
 }
