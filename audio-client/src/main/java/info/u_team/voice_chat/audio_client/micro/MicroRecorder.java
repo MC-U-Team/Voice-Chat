81b150c919de37fe0645d1d5dd8751f3bb73af07
HyCraftHD <hycrafthd@live.de>
2020-02-28 19:48:36 +0100

    Add volume controller

--- audio-client/src/main/java/info/u_team/voice_chat/audio_client/micro/MicroRecorder.java
+++ audio-client/src/main/java/info/u_team/voice_chat/audio_client/micro/MicroRecorder.java
@@ -1,5 +1,6 @@
 package info.u_team.voice_chat.audio_client.micro;
 
+import java.nio.*;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
 
@@ -17,6 +18,7 @@ public class MicroRecorder implements NoExceptionCloseable {
 	private final MicroData microData;
 	private final Consumer<byte[]> opusPacketConsumer;
 	private final IOpusEncoder encoder;
+	private double volume;
 	
 	private volatile boolean send;
 	
@@ -24,6 +26,7 @@ public class MicroRecorder implements NoExceptionCloseable {
 		this.microData = microData;
 		this.opusPacketConsumer = opusPacketConsumer;
 		this.encoder = new PcmOpusEncoder(48000, 2, 20, 64000, Opus.OPUS_SIGNAL_VOICE, 1000);
+		volume = 1;
 	}
 	
 	public void start() {
@@ -34,15 +37,30 @@ public class MicroRecorder implements NoExceptionCloseable {
 		executor.execute(() -> {
 			final byte[] buffer = new byte[960 * 2 * 2];
 			while (send && microData.isAvailable()) {
-				opusPacketConsumer.accept(encoder.encode(microData.read(buffer)));
+				opusPacketConsumer.accept(encoder.encode(adjustVolume(microData.read(buffer), volume)));
 			}
 			ThreadUtil.execute(5, 20, () -> opusPacketConsumer.accept(encoder.silence()));
 		});
 	}
 	
+	private byte[] adjustVolume(byte[] pcm, double volume) {
+		if (Math.abs(volume - 1) < 0.001) {
+			return pcm;
+		}
+		final ShortBuffer shortBuffer = ByteBuffer.wrap(pcm).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
+		for (int index = 0; index < shortBuffer.capacity(); index++) {
+			shortBuffer.put(index, (short) (shortBuffer.get(index) * volume));
+		}
+		return pcm;
+	}
+	
 	public void stop() {
 		send = false;
 		microData.flush();
+	}
+	
+	public void setVolume(double volume) {
+		this.volume = volume;
 	}
 	
 	public boolean isSending() {
