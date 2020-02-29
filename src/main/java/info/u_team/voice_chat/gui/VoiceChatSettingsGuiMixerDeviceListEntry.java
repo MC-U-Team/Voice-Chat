866d55e3a9787fd04b072d939cb1271c8e147a1e
HyCraftHD <hycrafthd@live.de>
2020-02-29 05:05:51 +0100

    Remove unused class

--- src/main/java/info/u_team/voice_chat/gui/VoiceChatSettingsGuiMixerDeviceListEntry.java
+++ src/main/java/info/u_team/voice_chat/gui/VoiceChatSettingsGuiMixerDeviceListEntry.java
@@ -1,8 +1,8 @@
 package info.u_team.voice_chat.gui;
 
-import info.u_team.music_player.gui.BetterScrollableListEntry;
+import info.u_team.u_team_core.gui.elements.ScrollableListEntry;
 
-class VoiceChatSettingsGuiMixerDeviceListEntry extends BetterScrollableListEntry<VoiceChatSettingsGuiMixerDeviceListEntry> {
+class VoiceChatSettingsGuiMixerDeviceListEntry extends ScrollableListEntry<VoiceChatSettingsGuiMixerDeviceListEntry> {
 	
 	private final String mixerName;
 	
@@ -17,6 +17,13 @@ class VoiceChatSettingsGuiMixerDeviceListEntry extends BetterScrollableListEntry
 	
 	public String getMixerName() {
 		return mixerName;
+	}
+	
+	@SuppressWarnings({ "deprecation" })
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		list.setSelected(this);
+		return super.mouseClicked(mouseX, mouseY, button);
 	}
 	
 }
