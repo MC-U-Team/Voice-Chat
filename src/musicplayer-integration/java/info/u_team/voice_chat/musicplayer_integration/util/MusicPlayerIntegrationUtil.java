25d7be80b95bb76b12a7f230f5344a280e27f9cd
HyCraftHD <hycrafthd@live.de>
2020-02-29 06:04:33 +0100

    Rename class

--- src/musicplayer-integration/java/info/u_team/voice_chat/musicplayer_integration/util/MusicPlayerIntegrationUtil.java
+++ src/musicplayer-integration/java/info/u_team/voice_chat/musicplayer_integration/util/MusicPlayerIntegrationUtil.java
@@ -5,10 +5,14 @@ import java.util.function.*;
 import info.u_team.music_player.musicplayer.MusicPlayerManager;
 import net.minecraftforge.fml.ModList;
 
-public class MusicPlayerSettingsUtil {
+public class MusicPlayerIntegrationUtil {
+	
+	public static boolean isMusicPlayerInstalled() {
+		return ModList.get().isLoaded("musicplayer");
+	}
 	
 	public static boolean isShowIngameMenueOverlay() {
-		if (ModList.get().isLoaded("musicplayer")) {
+		if (isMusicPlayerInstalled()) {
 			final Supplier<BooleanSupplier> supplier = () -> () -> MusicPlayerManager.getSettingsManager().getSettings().isShowIngameMenueOverlay(); // Use the supplier method to prevent loading the classes
 			return supplier.get().getAsBoolean();
 		} else {
