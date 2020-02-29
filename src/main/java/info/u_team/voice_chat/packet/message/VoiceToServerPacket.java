d9a5f4843f7f6c22a147fcdededc066971f100ae
HyCraftHD <hycrafthd@live.de>
2020-02-29 06:28:05 +0100

    Send to all except for voice packets

--- src/main/java/info/u_team/voice_chat/packet/message/VoiceToServerPacket.java
+++ src/main/java/info/u_team/voice_chat/packet/message/VoiceToServerPacket.java
@@ -41,10 +41,7 @@ public class VoiceToServerPacket {
 			}
 			
 			// TODO currently send the data to everybody even the sender
-			// VoiceServerManager.getServer().sendAllExcept(new VoiceToClientPacket(playerData.getId(), message.opusPacket),
-			// player);
-			
-			VoiceServerManager.getServer().sendAll(new VoiceToClientPacket(playerData.getId(), message.opusPacket));
+			VoiceServerManager.getServer().sendAllExcept(new VoiceToClientPacket(playerData.getId(), message.opusPacket), player);
 		}
 	}
 }
