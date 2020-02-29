6b699203e086ce5f80a4606ff7f122f3a44fdea8
HyCraftHD <hycrafthd@live.de>
2020-02-29 06:46:02 +0100

    Add sign verifier

--- src/main/java/info/u_team/voice_chat/VoiceChatMod.java
+++ src/main/java/info/u_team/voice_chat/VoiceChatMod.java
@@ -2,6 +2,7 @@ package info.u_team.voice_chat;
 
 import org.apache.logging.log4j.*;
 
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.voice_chat.config.*;
 import info.u_team.voice_chat.dependency.DependencyManager;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -16,6 +17,7 @@ public class VoiceChatMod {
 	public static final Logger LOGGER = LogManager.getLogger();
 	
 	public VoiceChatMod() {
+		JarSignVerifier.checkSigned(MODID);
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
 		ModLoadingContext.get().registerConfig(Type.CLIENT, ClientConfig.CONFIG);
 		DependencyManager.construct();
