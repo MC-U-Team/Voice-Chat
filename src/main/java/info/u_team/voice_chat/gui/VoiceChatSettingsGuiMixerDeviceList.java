dd10ec891ca11acf157b6e02beefe08cb0f12111
HyCraftHD <hycrafthd@live.de>
2020-02-29 05:04:58 +0100

    Add basic list for mixer

---
+++ src/main/java/info/u_team/voice_chat/gui/VoiceChatSettingsGuiMixerDeviceList.java
@@ -0,0 +1,29 @@
+package info.u_team.voice_chat.gui;
+
+import java.util.function.*;
+
+import javax.sound.sampled.Line;
+
+import info.u_team.u_team_core.gui.elements.ScrollableList;
+import info.u_team.voice_chat.audio_client.util.AudioUtil;
+
+public class VoiceChatSettingsGuiMixerDeviceList extends ScrollableList<VoiceChatSettingsGuiMixerDeviceListEntry> {
+	
+	private final Consumer<String> mixerSetter;
+	
+	public VoiceChatSettingsGuiMixerDeviceList(int width, int height, int top, int bottom, int left, int right, Supplier<Line.Info> lineInfoGetter, Supplier<String> mixerGetter, Consumer<String> mixerSetter) {
+		super(width, height, top, bottom, left, right, 20, 20, 5);
+		this.mixerSetter = mixerSetter;
+		AudioUtil.findAudioDevices(lineInfoGetter.get()).stream().map(VoiceChatSettingsGuiMixerDeviceListEntry::new).peek(entry -> {
+			if (entry.getMixerName().equals(mixerGetter.get())) {
+				super.setSelected(entry);
+			}
+		}).forEach(this::addEntry);
+	}
+	
+	@Override
+	public void setSelected(VoiceChatSettingsGuiMixerDeviceListEntry entry) {
+		super.setSelected(entry);
+		mixerSetter.accept(entry.getMixerName());
+	}
+}
