27c3fc1f413e36adc42200df9bda976d8bb12b0e
HyCraftHD <hycrafthd@live.de>
2020-02-29 04:18:28 +0100

    Add config options to the config

--- src/main/java/info/u_team/voice_chat/config/ClientConfig.java
+++ src/main/java/info/u_team/voice_chat/config/ClientConfig.java
@@ -21,14 +21,25 @@ public class ClientConfig {
 	}
 	
 	public final ConfigValue<String> microphoneValue;
+	public final IntValue microphoneVolumeValue;
+	
 	public final ConfigValue<String> speakerValue;
+	public final IntValue speakerVolumeValue;
 	
 	public final IntValue bitrateValue;
 	
 	private ClientConfig(Builder builder) {
 		builder.comment("Voice settings").push("client");
+		builder.push("microphone");
 		microphoneValue = builder.comment("Which microphone should be used. If empty or microphone not found the default one will be used. If no microphone is found the mod cannot record voice data.").define("microphone", "");
+		microphoneVolumeValue = builder.comment("The volume of the microphone in a linear range").defineInRange("volume", 100, 0, 150);
+		builder.pop();
+		
+		builder.push("speaker");
 		speakerValue = builder.comment("Which speaker should be used. If empty or speaker not found the default one will be used. If no speaker is found the mod cannot play voice data.").define("speaker", "");
+		speakerVolumeValue = builder.comment("The volume of the speaker in a linear range").defineInRange("volume", 100, 0, 150);
+		builder.pop();
+		
 		bitrateValue = builder.comment("The bitrate your voice packets are send to the server. If packet loss occurs try to lower the bitrate.").defineInRange("bitrate", 64000, 8000, 128000);
 		builder.pop();
 	}
